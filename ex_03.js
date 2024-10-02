465f66c8e7ae88d7a5b21360fba81861706baba9
B3Y0NDER <dine.terzi@epitech.digital>
2024-10-02 12:27:22 +0200

    Update ex_03.js

--- ex_03.js
+++ ex_03.js
@@ -1,1 +1,11 @@
-
+function Hamid(peopleAtBusStops, busSeats){
+let totalPassagers=0;
+for ( i = 0; i < peopleAtBusStops.lenght; i+1){
+totalPassagers += peopleAtBusStops[i]; 
+if (totalPassagers >= busSeats) {
+return i;
+}
+}
+return -1;
+}
+console.log(Hamid([1, 3, 10, 1], 12));
