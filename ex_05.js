0a43da68acc2efaa534148bb60b315ce4337ce0c
B3Y0NDER <dine.terzi@epitech.digital>
2024-10-02 14:47:42 +0200

    Update ex_05.js

--- ex_05.js
+++ ex_05.js
@@ -1,1 +1,11 @@
+function getHouses(){
+const houses = new Map();
+houses.set("Apartment 75008",90)
+houses.set("Apartment 94300",105)
+houses.set("Apartment 92300",140)
+houses.set("Apartment 75015",65)
+return houses;
+}
 
+console.log(getHouses());
+//marche sur vs code mais sur jsfiddle
