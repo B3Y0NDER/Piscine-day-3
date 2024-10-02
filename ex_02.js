7e0dfb7e3ee4355f1c496c144a6c0c30a7d08796
B3Y0NDER <dine.terzi@epitech.digital>
2024-10-02 12:00:06 +0200

    Update ex_02.js

--- ex_02.js
+++ ex_02.js
@@ -1,8 +1,10 @@
-let housingList = ("Flat", "House", "Hut", "House", "Igloo")
+let housingList = ("Flat", "House", "Hut", "House", "Igloo", "flat","FLAT","HOUSE","hut","HUT","house","IGLOO","igloo")
 function findHousing(housingToFind, housingList){
 for(let i = 0; i < housingList.lenght;i + 1){
-if (housingList(i)===housingToFind)
-return (i,housingList(i));
+if (housingList[i].toLowerCase() === housingToFind.toLowerCase()){
+ return [i, housingList[i]];
 }
-return null}
+return null
+}
 let result = findHousing("Igloo",housingList);
+}
