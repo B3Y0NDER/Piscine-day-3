2c10986630f4dc6b1dc1f47d10cef9271f82c1ec
B3Y0NDER <dine.terzi@epitech.digital>
2024-10-02 17:24:22 +0200

    Update ex_06.js

--- ex_06.js
+++ ex_06.js
@@ -1,1 +1,11 @@
-
+const adresses = [ ["45 avenue des tulipes", "92130 Issy-les-Moulineaux"],["95 avenue parmentier", "75011 Paris"],["24 Rue Pasteur", "92400 Courbevoie"],["Champ de Mars, 5 Av. Anatole France", "75007 Paris"],["Pl. Louis-Armand", "75571 Paris"]
+];
+function findAddresses(searchString) {
+for (let i = 0; i < adresses.length; i++) {
+const fullAddress = adresses[i].join(", ");//j'ai fait une faute sur adresse car sinon le code ne marchait pas puisqu'il a déja été défini
+if (fullAddress.includes(searchString)) {
+      console.log(fullAddress);
+}
+}
+}
+findAddresses("Paris");
